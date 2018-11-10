f52d5b4c690925fb70064b31b8f9a8d95a1c9cad
SathyaBhat <sathya@sathyasays.com>
2018-11-10 17:31:47 +0530

    update links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,16 +17,17 @@ const Navbar = () => (
         <Link className="navbar-item" to="/about">
           About
         </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
+        <a 
+          className="navbar-item"
+          href="https://meetup.com/awsugblr/"
+          target="_blank"
+          >Meetup Page</a>
       </div>
       <div className="navbar-end">
         <a
           className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+          href="https://github.com/awsugblr/awsugblr"
           target="_blank"
-          rel="noopener noreferrer"
         >
           <span className="icon">
             <img src={github} alt="Github" style={{ width: '150px' }} />
