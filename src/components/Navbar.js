821e84549c879f5854efcc27fe32bcf02eaff0d4
SathyaBhat <sathya@sathyasays.com>
2018-11-10 17:13:27 +0530

    add awsugblr logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import logo from '../img/awsugblr.jpeg'
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
