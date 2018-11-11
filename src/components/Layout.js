3a788d0bc3737bb00202c2f0826f5eae51a26ab3
SathyaBhat <sathya@sathyasays.com>
2018-11-11 22:13:18 +0530

    update title

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="AWS Users Group, Bengaluru" />
     <Navbar />
     <div>{children}</div>
   </div>
