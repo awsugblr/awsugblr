cd462196ac615daac123432f5725d656e1369c12
SathyaBhat <sathya@sathyasays.com>
2018-11-18 17:43:44 +0530

    add social network logos

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,12 @@ import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
 import logo from '../img/awsugblr.jpeg'
+import twitter_logo from '../img/twitter.svg'
+import facebook_logo from '../img/facebook.svg'
+import linkedin_logo from '../img/linkedin.svg'
+import telegram_logo from '../img/telegram.svg'
+import rss_logo from '../img/rss.svg'
+
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
@@ -23,6 +29,68 @@ const Navbar = () => (
           target="_blank"
           >Meetup Page</a>
       </div>
+
+      <div className="navbar-end">
+        <a
+          className="navbar-item"
+          href="https://twitter.com/awsugblr/"
+          target="_blank"
+        >
+          <span className="icon">
+            <img src={twitter_logo} alt="Twitter" style={{ width: '150px' }} />
+          </span>
+        </a>
+      </div>
+
+      <div className="navbar-end">
+        <a
+          className="navbar-item"
+          href="https://facebook.com/awsugblr/"
+          target="_blank"
+        >
+          <span className="icon">
+            <img src={facebook_logo} alt="Facebook" style={{ width: '150px' }} />
+          </span>
+        </a>
+      </div>
+
+      <div className="navbar-end">
+        <a
+          className="navbar-item"
+          href="https://linkedin.com/in/awsugblr"
+          target="_blank"
+        >
+          <span className="icon">
+            <img src={linkedin_logo} alt="LinkedIn" style={{ width: '150px' }} />
+          </span>
+        </a>
+      </div>
+
+      <div className="navbar-end">
+        <a
+          className="navbar-item"
+          href="https://t.me/awsugblr"
+          target="_blank"
+        >
+          <span className="icon">
+            <img src={telegram_logo} alt="Telegram" style={{ width: '150px' }} />
+          </span>
+        </a>
+      </div>
+
+      <div className="navbar-end">
+        <a
+          className="navbar-item"
+          href="https://awsugblr.in/rss.xml"
+          target="_blank"
+        >
+          <span className="icon">
+            <img src={rss_logo} alt="RSS" style={{ width: '150px' }} />
+          </span>
+        </a>
+      </div>
+
+
       <div className="navbar-end">
         <a
           className="navbar-item"
@@ -34,6 +102,7 @@ const Navbar = () => (
           </span>
         </a>
       </div>
+
     </div>
   </nav>
 )
