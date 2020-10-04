923e984109364442110ac9a14208beda45aba8e0
Saifur Rahman Mohsin <mohsin92@me.com>
2020-10-04 21:26:31 +0530

    Bugfix: Remove about us from blog post listings. (#329)

--- src/templates/index.js
+++ src/templates/index.js
@@ -15,7 +15,7 @@ export default class IndexPage extends React.Component {
             <div className="content">
               <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
             </div>
-            {group.map(({ node: post }) => (
+            {group.filter(post => post.node.frontmatter.templateKey === 'blog-post').map(({ node: post }) => (
                 <div
                   className="content"
                   style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
